501a513d88fa7e0464d1acc2b58ebbb9e806a24f
Jaehyun Koo <koosaga@gmail.com>
2016-11-19 20:57:33 +0900

    added NTT

--- Library/math.cpp
+++ Library/math.cpp
@@ -83,7 +83,7 @@ namespace fft{
 			}
 		}
 	}
-	vector<int> multiply(vector<int> &v, vector<int> &w){
+	vector<lint> multiply(vector<lint> &v, vector<lint> &w){
 		vector<base> fv(v.begin(), v.end()), fw(w.begin(), w.end());
 		int n = 1;
 		while(n < max(v.size(), w.size())) n <<= 1;
@@ -94,11 +94,11 @@ namespace fft{
 		fft(fw, 0);
 		for(int i=0; i<n; i++) fv[i] *= fw[i];
 		fft(fv, 1);
-		vector<int> ret(n);
+		vector<lint> ret(n);
 		for(int i=0; i<n; i++) ret[i] = round(fv[i].real());
 		return ret;
 	}
-	vector<int> power(vector<int> &v){
+	vector<lint> power(vector<lint> &v){
 		vector<base> fv(v.begin(), v.end());
 		int n = 1;
 		while(n < v.size()) n <<= 1;
@@ -107,13 +107,65 @@ namespace fft{
 		fft(fv, 0);
 		for(int i=0; i<n; i++) fv[i] *= fv[i];
 		fft(fv, 1);
-		vector<int> ret(n);
+		vector<lint> ret(n);
 		for(int i=0; i<n; i++) ret[i] = round(fv[i].real());
 		return ret;
 	}
-};
+}
 
-struct miller_rabin{
+namespace nt_fft{
+	const int prr = 3;
+	void fft(vector<int> &v, bool inv){
+		vector<int> w(v.size());
+		for(int i=2; i<=v.size(); i<<=1){
+			int bsz = v.size() / i;
+			int ang = ipow(prr, (mod - 1) / i);
+			if(inv) ang = ipow(ang, mod-2);
+			for(int j=0; j<bsz; j++){
+				for(int k=0; k<i; k++){
+					w[k] = v[j + k * bsz];
+				}
+				lint cur = 1;
+				for(int k=0; k<i/2; k++){
+					lint a = w[2*k], b = cur * w[2*k+1] % mod;
+					v[j + k * bsz] = (a + b) % mod;
+					v[j + k * bsz + v.size()/2] = (a - b + mod) % mod;
+					cur = (cur * ang) % mod;
+				}
+			}
+		}
+		if(inv){
+			lint mul = ipow(v.size(), mod-2);
+			for(int i=0; i<v.size(); i++){
+				v[i] = (mul * v[i]) % mod;
+			}
+		}
+	}
+	vector<int> multiply(vector<int> a, vector<int> b){
+		int w = 1;
+		while(w < max(a.size(), b.size())) w <<= 1;
+		w <<= 1;
+		a.resize(w);
+		b.resize(w);
+		fft(a, 0); 
+		fft(b, 0);
+		for(int i=0; i<w; i++) a[i] = (1ll * a[i] * b[i]) % mod;
+		fft(a, 1);
+		return a;
+	}
+	vector<int> power(vector<int> a){
+		int w = 1;
+		while(w < a.size()) w <<= 1;
+		w <<= 1;
+		a.resize(w);
+		fft(a, 0);
+		for(int i=0; i<w; i++) a[i] = (1ll * a[i] * a[i]) % mod;
+		fft(a, 1);
+		return a;
+	}
+}
+
+namespace miller_rabin{
 	lint mul(lint a, lint b, lint p){
 		lint ret = 0;
 		while(b){
@@ -150,7 +202,7 @@ struct miller_rabin{
 		if(x < 40) return 0;
 		return 1;
 	}
-}isprime;
+}
 
 namespace count_digits{
 	const int MAXN = 17;
